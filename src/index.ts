f5db3f4bdcfa4716b887f0cebde633458e810182
Gid.Lamim <gid.lamim@gmail.com>
2022-07-18 15:52:10 -0300

    create a server using express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("executando");
+import express, {Request, Response, NextFunction}  from "express";
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction)=>{
+ res.status(200).send({foo: 'bar'});
+});
+
+app.listen(3000, ()=>{ // to access this:  http://localhost:3000
+    console.log('porta 3000 ');
+})
+// this way, we can create a server very easily 
