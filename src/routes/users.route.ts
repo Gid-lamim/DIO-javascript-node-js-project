2fb73f33dc7fb2a148f7df9a1c7583b6f552ec3d
Gid.Lamim <gid.lamim@gmail.com>
2022-07-19 11:28:59 -0300

    add edit user script

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -19,7 +19,7 @@ userRoute.get('/users', (req:Request, res:Response, next:NextFunction)=>{
 
 //this route will be used o locate a user by its id.
 //with express, we use ':' to say that the next part of the url is dynamic(variable)
-userRoute.get('/users/:uuid', (req:Request, res:Response, next:NextFunction)=>{
+userRoute.get('/users/:uuid', (req:Request<{uuid:string}>, res:Response, next:NextFunction)=>{
     //req.params will give me the value entered in the url.
     //for example: http:localhost:3000/users/1234 will have 1234 as uuid
     const uuid = req.params.uuid;
@@ -30,8 +30,13 @@ userRoute.get('/users/:uuid', (req:Request, res:Response, next:NextFunction)=>{
 //to use POST we need postman or other apps. We can't use the browser.
 userRoute.post('/users', (req:Request, res:Response, next:NextFunction)=>{
     const newUser = req.body;
-    
     res.status(201).send(newUser);
+});
+
+
+userRoute.put('/users/:uuid', (req:Request <{uuid: string}>, res:Response, next:NextFunction)=>{
+    const uuid = req.params.uuid;
+    res.status(200).send({uuid});
 });
 
 export default userRoute;
