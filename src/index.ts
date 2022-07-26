07c99ce42acdd28082403c7b9a937bc8e473850e
Gid.Lamim <gid.lamim@gmail.com>
2022-07-25 21:24:23 -0300

    alter the express routes order
    Express is sensible when it comes to the order of routes. It was updated to reflect the needs of the authentication routes.

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,14 @@ app.use(express.json()); //this will enable the code to understand json bodies
 app.use(express.urlencoded({extended:true}));
 
 //Routes imports
-//app.use(basicAuthenticationMiddleware);
+// here the order counts. 
+app.use(statusRoute); // this route doesn't need authentication
+app.use(authorizationRoute); // these route are used to get or validadte a token. 
 app.use(jwtbearerAuthenticationMiddleware, userRoute); //this ensures that all user routes are authenticated. The access without a token is simply not allowed
-app.use(statusRoute);
-app.use(authorizationRoute);
+
+/* if I put app.use(authorizationRoute) before app.use(statusRoute),
+ it will ask for an authentication before we can check the api status*/
+
 
 //errorHandlers
 app.use(errorHandler); //this will take care of all errors
